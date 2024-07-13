ebdf514d13b22968f60147a6dfb22df5592ee558
Chamath963 <chamaththarupathi@gmail.com>
2024-07-13 18:04:05 +0530

    script.js

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     document.getElementById('join-telegram-group').addEventListener('click', function() {
+    colour #333
     // Retrieve the URL from the data-url attribute
     var url = this.getAttribute('data-url');
     
